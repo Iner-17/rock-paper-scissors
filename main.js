6e9a1c8dcab6dbba38432545fb50ce4f51f1dafe
iner-17 <inercollado52@gmail.com>
2020-08-26 21:22:04 +0900

    finished the game

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
   //Username will be printed to the top of the guess pic
-  let userName = prompt("Enter your name");
-  let otherName = userName.slice(1);
-  let firstLetter = userName.slice(0, 1);
-  document.querySelector(".player-name").textContent =
-    firstLetter.toUpperCase() + otherName.toLowerCase();
-});
+  //let userName = prompt("Enter your name");
+  // let otherName = userName.slice(1);
+  // let firstLetter = userName.slice(0, 1);
+  // document.querySelector(".player-name").textContent =
+  //   firstLetter.toUpperCase() + otherName.toLowerCase();
+
+  let playerGuess = document.querySelector('.player-guess');
+  let botGuess = document.querySelector('.bot-guess');
+
+  function displayRock() {
+    playerGuess.textContent = 'Rock';
+    botGuess.textContent = '. . .';
+  }
+
+  function displayPaper() {
+    playerGuess.textContent = 'Paper';
+    botGuess.textContent = '. . .';
+  }
+
+  function displayScissors() {
+    playerGuess.textContent = 'Scissors';
+    botGuess.textContent = '. . .';
+  }
+
+  function showResult() {
+    if (playerGuess.textContent === 'Rock') {
+      botGuess.textContent = 'Paper';
+      alert('Bot: you lose better luck next time rocker man')
+    } else if (playerGuess.textContent === 'Paper') {
+      botGuess.textContent = 'Scissors';
+      alert('Bot: Im gonna cut that paper with my mighty scissors')
+    } else {
+      botGuess.textContent = 'Rock';
+      alert('Bot: I have rock you lose milkdrnker')
+    }
+  }
